ea480471ddded06a34a4004d511fee4a143eabce
Marcos <marcos.cristiano.af@gmail.com>
2014-04-18 19:55:20 -0300

    Iniciando Projeto

---
+++ src/main/webapp/resources/js/function.js
@@ -0,0 +1,69 @@
+function clearField(idField) {
+	var inputs = document.getElementsByTagName("input");
+	var selects = document.getElementsByTagName("select");
+	var labels = document.getElementsByTagName("label");
+	/*var textAreas = document.getElementsByTagName("label");*/
+
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].id.indexOf(idField) >= 0) {
+			inputs[i].value = "";
+			break;
+		}
+	}
+
+	for (var i = 0; i < selects.length; i++) {
+		if (selects[i].id.indexOf(idField) >= 0) {
+			selects[i].value = "";
+			break;
+		}
+	}
+
+	for (var i = 0; i < labels.length; i++) {
+		if (labels[i].id.indexOf(idField) >= 0) {
+			labels[i].innerHTML = "...";
+			break;
+		}
+	}
+}
+
+function clearFields(elementId) {
+	var element = document.getElementById(elementId);
+	var inputs = element.getElementsByTagName("input");
+	var selects = element.getElementsByTagName("select");
+	var labels = document.getElementsByTagName("label");
+
+	/*var textAreas = document.getElementsByTagName("label");*/
+
+	for (var i = 0; i < inputs.length; i++) {
+
+		if (inputs[i].id.indexOf('ipt') >= 0) {
+			inputs[i].value = "";
+		}
+	}
+	
+	for (var i = 0; i < selects.length; i++) {
+
+		if (selects[i].id.indexOf('ipt') >= 0) {
+			selects[i].value = "";
+		}
+	}
+	
+	for (var i = 0; i < labels.length; i++) {
+
+		if (labels[i].id.indexOf('ipt') >= 0) {
+			labels[i].innerHTML = "...";
+		}
+	}
+	
+	closeMessPanelIfOpen();
+}
+
+
+function closeMessPanelIfOpen() {
+	try {
+		document.getElementById('templateForm:msgsForm').children[0].children[0]
+				.click();
+	} catch (err) {
+
+	}
+}
